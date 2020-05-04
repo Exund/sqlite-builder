94a0075b9e11e7d9b49c17639e6633a738ce4fd6
Exund <SylverProfessionnel@gmail.com>
2020-05-04 15:44:47 +0200

    Removed better-sqlite dependency + correct export

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-export { Table } from "./lib/Table";
+/*export { Table } from "./lib/Table";
 export { Column, ColumnType } from "./lib/Column";
 export * as TableConstraints from "./lib/TableConstraints";
 export * as ColumnConstraints from "./lib/ColumnConstraints";
 export * from "./lib/ForeignKey";
-export * from "./lib/ConstraintConflict";
+export * from "./lib/ConstraintConflict";*/
+
+module.exports = {
+    ...require("./lib/Table"),
+    ...{ Column, ColumnType } = require("./lib/Column"),
+    TableConstraints: require("./lib/TableConstraints"),
+    ColumnConstraints: require("./lib/ColumnConstraints"),
+    ...require("./lib/ForeignKey"),
+    ...require("./lib/ConstraintConflict")
+};
